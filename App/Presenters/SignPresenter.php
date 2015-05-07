009b34b133d6e35267535cd754da23ef354a7526
Jiri Travnicek <jiri.travnicek@outlook.com>
2015-05-07 19:38:42 +0200

    redirect to events after login

--- App/Presenters/SignPresenter.php
+++ App/Presenters/SignPresenter.php
@@ -23,7 +23,7 @@ class SignPresenter extends BasePresenter
 	public function actionOut()
 	{
 		$this->user->logout();
-		$this->redirect(':Homepage:default');
+		$this->redirect(':Event:default');
 		$this->flashMessage('admin-sign.flashMessage.logoutSuccess', 'success');
 	}
 
